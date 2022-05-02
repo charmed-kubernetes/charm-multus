1692b1e0f2a20120c58ccd0a66d5480ee5d1e93b
Adam Dyess <adam.dyess@canonical.com>
2022-05-02 14:39:32 -0500

    lint, and inclusivity check on PR

--- src/charm.py
+++ src/charm.py
@@ -14,16 +14,15 @@ log = logging.getLogger()
 class MultusCharm(CharmBase):
     def __init__(self, framework, key):
         super().__init__(framework, key)
-        self.multus_image = OCIImageResource(self, 'multus-image')
-        self.nadm_image = OCIImageResource(self,
-                                           'net-attach-def-manager-image')
+        self.multus_image = OCIImageResource(self, "multus-image")
+        self.nadm_image = OCIImageResource(self, "net-attach-def-manager-image")
         self.framework.observe(self.on.install, self.set_pod_spec)
         self.framework.observe(self.on.upgrade_charm, self.set_pod_spec)
         self.framework.observe(self.on.config_changed, self.set_pod_spec)
 
     def set_pod_spec(self, event):
         if not self.model.unit.is_leader():
-            log.info('Not a leader, skipping set_pod_spec')
+            log.info("Not a leader, skipping set_pod_spec")
             self.model.unit.status = ActiveStatus()
             return
 
@@ -34,11 +33,9 @@ class MultusCharm(CharmBase):
             self.model.unit.status = e.status
             return
 
-        net_attach_defs_str = self.model.config.get(
-            'network-attachment-definitions', ''
-        )
+        net_attach_defs_str = self.model.config.get("network-attachment-definitions", "")
         invalid_net_attach_def_status = BlockedStatus(
-            'network-attachment-definitions config is invalid, see debug-log'
+            "network-attachment-definitions config is invalid, see debug-log"
         )
         try:
             net_attach_defs = list(yaml.safe_load_all(net_attach_defs_str))
@@ -48,152 +45,136 @@ class MultusCharm(CharmBase):
             return
 
         for net_attach_def in net_attach_defs:
-            if net_attach_def.get('apiVersion') != 'k8s.cni.cncf.io/v1':
-                log.error('network-attachment-definitions config is invalid:'
-                          + ' apiVersion must be k8s.cni.cncf.io/v1')
+            if net_attach_def.get("apiVersion") != "k8s.cni.cncf.io/v1":
+                log.error(
+                    "network-attachment-definitions config is invalid:"
+                    + " apiVersion must be k8s.cni.cncf.io/v1"
+                )
                 self.model.unit.status = invalid_net_attach_def_status
                 return
-            if net_attach_def.get('kind') != 'NetworkAttachmentDefinition':
-                log.error('network-attachment-definitions config is invalid:'
-                          + ' kind must be NetworkAttachmentDefinition')
+            if net_attach_def.get("kind") != "NetworkAttachmentDefinition":
+                log.error(
+                    "network-attachment-definitions config is invalid:"
+                    + " kind must be NetworkAttachmentDefinition"
+                )
                 self.model.unit.status = invalid_net_attach_def_status
                 return
-            if not net_attach_def.get('metadata', {}).get('name'):
-                log.error('network-attachment-definitions config is invalid:'
-                          + ' metadata.name is required')
+            if not net_attach_def.get("metadata", {}).get("name"):
+                log.error(
+                    "network-attachment-definitions config is invalid:"
+                    + " metadata.name is required"
+                )
                 self.model.unit.status = invalid_net_attach_def_status
                 return
-            if not net_attach_def.get('spec', {}).get('config'):
-                log.error('network-attachment-definitions config is invalid:'
-                          + ' spec.config is required')
+            if not net_attach_def.get("spec", {}).get("config"):
+                log.error(
+                    "network-attachment-definitions config is invalid:"
+                    + " spec.config is required"
+                )
                 self.model.unit.status = invalid_net_attach_def_status
                 return
 
         for net_attach_def in net_attach_defs:
-            net_attach_def['metadata'].setdefault('namespace', self.model.name)
+            net_attach_def["metadata"].setdefault("namespace", self.model.name)
 
-        self.model.unit.status = MaintenanceStatus('Setting pod spec')
+        self.model.unit.status = MaintenanceStatus("Setting pod spec")
         pod_spec = {
-            'version': 3,
-            'containers': [
+            "version": 3,
+            "containers": [
                 {
-                    'name': 'kube-multus',
-                    'imageDetails': multus_image_details,
-                    'command': ['/entrypoint.sh'],
-                    'args': [
-                        '--multus-conf-file=auto',
-                        '--cni-version=0.3.1'
-                    ],
-                    'volumeConfig': [
+                    "name": "kube-multus",
+                    "imageDetails": multus_image_details,
+                    "command": ["/entrypoint.sh"],
+                    "args": ["--multus-conf-file=auto", "--cni-version=0.3.1"],
+                    "volumeConfig": [
                         {
-                            'name': 'cni',
-                            'mountPath': '/host/etc/cni/net.d',
-                            'hostPath': {
-                                'path': '/etc/cni/net.d'
-                            }
+                            "name": "cni",
+                            "mountPath": "/host/etc/cni/net.d",
+                            "hostPath": {"path": "/etc/cni/net.d"},
                         },
                         {
-                            'name': 'cnibin',
-                            'mountPath': '/host/opt/cni/bin',
-                            'hostPath': {
-                                'path': '/opt/cni/bin'
-                            }
-                        }
+                            "name": "cnibin",
+                            "mountPath": "/host/opt/cni/bin",
+                            "hostPath": {"path": "/opt/cni/bin"},
+                        },
                     ],
-                    'kubernetes': {
-                        'securityContext': {
-                            'privileged': True
-                        }
-                    }
+                    "kubernetes": {"securityContext": {"privileged": True}},
                 },
                 {
-                    'name': 'net-attach-def-manager',
-                    'imageDetails': nadm_image_details,
-                    'volumeConfig': [{
-                        'name': 'config',
-                        'mountPath': '/config',
-                        'files': [{
-                            'path': 'manifest.yaml',
-                            'content': yaml.safe_dump_all(net_attach_defs) or
-                                '# empty'
-                        }]
-                    }]
-                }
-            ],
-            'serviceAccount': {
-                'roles': [{
-                    'global': True,
-                    'rules': [
+                    "name": "net-attach-def-manager",
+                    "imageDetails": nadm_image_details,
+                    "volumeConfig": [
                         {
-                            'apiGroups': ['k8s.cni.cncf.io'],
-                            'resources': ['*'],
-                            'verbs': ['*']
-                        },
-                        {
-                            'apiGroups': [''],
-                            'resources': [
-                                'pods',
-                                'pods/status'
-                            ],
-                            'verbs': [
-                                'get',
-                                'update'
-                            ]
-                        }
-                    ]
-                }]
-            },
-            'kubernetesResources': {
-                'pod': {
-                    'hostNetwork': True
-                },
-                'customResourceDefinitions': [{
-                    'name': 'network-attachment-definitions.k8s.cni.cncf.io',
-                    'spec': {
-                        'group': 'k8s.cni.cncf.io',
-                        'scope': 'Namespaced',
-                        'names': {
-                            'plural': 'network-attachment-definitions',
-                            'singular': 'network-attachment-definition',
-                            'kind': 'NetworkAttachmentDefinition',
-                            'shortNames': ['net-attach-def']
-                        },
-                        'versions': [{
-                            'name': 'v1',
-                            'served': True,
-                            'storage': True,
-                            'schema': {
-                                'openAPIV3Schema': {
-                                    'type': 'object',
-                                    'properties': {
-                                        'apiVersion': {
-                                            'type': 'string'
-                                        },
-                                        'kind': {
-                                            'type': 'string'
-                                        },
-                                        'metadata': {
-                                            'type': 'object'
-                                        },
-                                        'spec': {
-                                            'type': 'object',
-                                            'properties': {
-                                                'config': {
-                                                    'type': 'string'
-                                                }
-                                            }
-                                        }
-                                    }
+                            "name": "config",
+                            "mountPath": "/config",
+                            "files": [
+                                {
+                                    "path": "manifest.yaml",
+                                    "content": yaml.safe_dump_all(net_attach_defs) or "# empty",
                                 }
-                            }
-                        }],
+                            ],
+                        }
+                    ],
+                },
+            ],
+            "serviceAccount": {
+                "roles": [
+                    {
+                        "global": True,
+                        "rules": [
+                            {"apiGroups": ["k8s.cni.cncf.io"], "resources": ["*"], "verbs": ["*"]},
+                            {
+                                "apiGroups": [""],
+                                "resources": ["pods", "pods/status"],
+                                "verbs": ["get", "update"],
+                            },
+                        ],
                     }
-                }]
-            }
+                ]
+            },
+            "kubernetesResources": {
+                "pod": {"hostNetwork": True},
+                "customResourceDefinitions": [
+                    {
+                        "name": "network-attachment-definitions.k8s.cni.cncf.io",
+                        "spec": {
+                            "group": "k8s.cni.cncf.io",
+                            "scope": "Namespaced",
+                            "names": {
+                                "plural": "network-attachment-definitions",
+                                "singular": "network-attachment-definition",
+                                "kind": "NetworkAttachmentDefinition",
+                                "shortNames": ["net-attach-def"],
+                            },
+                            "versions": [
+                                {
+                                    "name": "v1",
+                                    "served": True,
+                                    "storage": True,
+                                    "schema": {
+                                        "openAPIV3Schema": {
+                                            "type": "object",
+                                            "properties": {
+                                                "apiVersion": {"type": "string"},
+                                                "kind": {"type": "string"},
+                                                "metadata": {"type": "object"},
+                                                "spec": {
+                                                    "type": "object",
+                                                    "properties": {"config": {"type": "string"}},
+                                                },
+                                            },
+                                        }
+                                    },
+                                }
+                            ],
+                        },
+                    }
+                ],
+            },
         }
         self.model.pod.set_spec(pod_spec)
         self.model.unit.status = ActiveStatus()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(MultusCharm)
